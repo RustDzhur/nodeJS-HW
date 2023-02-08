cadb2709e8eab6ffb30e828f9644d227c2dc3c27
Rustem Dzhuraiev <73857551+RustDzhur@users.noreply.github.com>
2023-02-08 23:56:13 +0100

    HW ready

--- contacts.js
+++ contacts.js
@@ -3,19 +3,54 @@ const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.resolve("./db/contacts.json");
 
+//Получаем контакты, 1. читаем. 2. парсим. 3. возвращаем распарсиній массив
+async function listContacts() {
+	try {
+		const data = await fs.readFile(contactsPath, "utf8");
+		const existingUsers = await JSON.parse(data);
+		console.log(existingUsers);
+		return existingUsers;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+//Получаем контакты, 1. читаем. 2. парсим. 3. находим по IDшнику
 async function getContactById(contactId) {
 	try {
 		const data = await fs.readFile(contactsPath, "utf8");
-		const getContact = JSON.parse(data).find(
-			obj => parseInt(obj.id) === parseInt(contactId)
-		);
-		return getContact;
+		const getContact = await JSON.parse(data).find(
+			obj => String(obj.id) === String(contactId)
+		)
+		console.log(`The user with id: ${contactId} succssesfuly got!!!`, getContact)
+		return getContact
 	} catch (error) {
 		console.log(error);
 		return undefined;
 	}
 }
 
+//Фильтруем контакт, 1. читаем. 2. парсим. 3. Фильтруем новый массив. 4. новым массив стрингифаем и записываем
+async function removeContact(contactId) {
+	try {
+		const data = await fs.readFile(contactsPath, "utf8");
+		const existingUsers = await JSON.parse(data);
+		const filterUser = await existingUsers.filter(
+			user => String(user.id) !== String(contactId)
+		);
+		const dataWriteNewObj = await fs.writeFile(
+			contactsPath,
+			JSON.stringify(filterUser),
+			"utf8"
+		);
+		console.log(`The user with id: ${contactId} succssesfuly removed!!!`)
+		return dataWriteNewObj;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+//Добавляем контакт, 1. читаем. 2. парсим. 3. распыляем новый массив. 4. новым массив стрингифаем и записываем
 async function addContact(name, email, phone) {
 	const newContact = {
 		id: uniqid(),
@@ -26,19 +61,22 @@ async function addContact(name, email, phone) {
 	try {
 		const getAllUsers = await fs.readFile(contactsPath, "utf8");
 		const existingUsers = await JSON.parse(getAllUsers);
-		const restNewUser = await [...existingUsers, newContact];
-		const dataAddNewUser = await fs.writeFile(
+		const addNewUser = await [...existingUsers, newContact];
+		const dataWriteNewUser = await fs.writeFile(
 			contactsPath,
-			JSON.stringify(restNewUser),
+			JSON.stringify(addNewUser),
 			"utf8"
 		);
-		return dataAddNewUser;
+		console.log(`The user succssesfuly added!!!`, newContact)
+		return dataWriteNewUser;
 	} catch (error) {
 		console.log(error);
 	}
 }
 
 module.exports = {
+	listContacts,
 	getContactById,
+	removeContact,
 	addContact,
 };
--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const { getContactById, addContact } = require("./contacts.js");
+const argv = require("yargs").argv;
+const {listContacts, getContactById, removeContact, addContact } = require("./contacts.js");
 
-getContactById(5).then(console.log)
 
-addContact('Rustem', 'dzhuraievrustem@gmail.com', '068578714')
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      await listContacts ()
+      break;
+
+    case "get":
+      await getContactById(id)
+      break;
+
+    case "add":
+      await addContact(name, email, phone)
+      break;
+
+    case "remove":
+      await removeContact(id)
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
